958a82efae0883c78ec9476d165572048596fa0a
NycolasCruz <nycolascruzsilva@gmail.com>
2024-08-04 18:40:55 -0300

    chore: eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+	{ files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	{
+		rules: {
+			"@typescript-eslint/no-unused-vars": "warn",
+			"react/react-in-jsx-scope": "off",
+		},
+	},
+	{
+		// Override configuration for files in the @types directory
+		files: ["**/@types/**/*"],
+		rules: {
+			"@typescript-eslint/no-unused-vars": "off",
+		},
+	},
+];

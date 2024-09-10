766078607722458b643de68fca5b79618274f9b3
NycolasCruz <nycolascruzsilva@gmail.com>
2024-09-10 15:03:06 -0300

    feat: initial layout configs

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,32 @@
+import { StatusBar, View } from "react-native";
+
 import { Slot } from "expo-router";
 
-import "@/styles/global.css";
+import { Loading } from "@/components/Loading";
+
+import {
+	useFonts,
+	Inter_400Regular,
+	Inter_500Medium,
+	Inter_600SemiBold,
+} from "@expo-google-fonts/inter";
+import "@/global/styles/global.css";
 
 export default function Layout() {
-	return <Slot />;
+	const [fontsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_500Medium,
+		Inter_600SemiBold,
+	});
+
+	if (!fontsLoaded) {
+		return <Loading />;
+	}
+
+	return (
+		<View className="flex-1 bg-zinc-950">
+			<StatusBar backgroundColor="transparent" barStyle="light-content" translucent />
+			<Slot />
+		</View>
+	);
 }

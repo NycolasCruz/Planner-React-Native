1ada639a5c31e713074b2b41818a5cfbc48ed32d
NycolasCruz <nycolascruzsilva@gmail.com>
2024-09-10 14:04:43 -0300

    chore: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
-import { fontFamily } from "./src/styles/fontFamily";
-import { colors } from "./src/styles/colors";
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	presets: [require("nativewind/preset")],
 	theme: {
-		extend: {
-			fontFamily,
-			colors,
-		},
+		extend: {},
 	},
 	plugins: [],
 };

6d8f84c39ce5af7234696976c561ac7ecf567749
NycolasCruz <nycolascruzsilva@gmail.com>
2024-09-17 14:47:04 -0300

    feat: main screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,28 @@
+import { useState } from "react";
 import { Image, Text, View } from "react-native";
 
 import { MapPin, Calendar, Settings2, UserRoundPlus, ArrowRight } from "lucide-react-native";
+
+import { StepForm } from "@/enums/StepForm";
 
 import { Separator } from "@/components/Separator";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 
+import background from "@/assets/background.png";
 import logo from "@/assets/logo.png";
 import { colors } from "@/global/colors";
 
 export default function App() {
-	// 11:30
+	const [stepForm, setStepForm] = useState(StepForm.tripDetails);
+
+	const isTripDetails = stepForm === StepForm.tripDetails;
 
 	return (
 		<View className="flex-1 items-center justify-center px-5">
 			<Image source={logo} className="h-8" resizeMode="contain" />
+
+			<Image source={background} className="absolute" />
 
 			<Text className="text-zinc-400 text-center text-lg mt-3">
 				Convide seus amigos e planeje sua{"\n"}próxima viagem
@@ -23,28 +31,32 @@ export default function App() {
 			<View className="w-full bg-zinc-900 rounded-xl border border-zinc-800 p-4 my-8">
 				<Input>
 					<MapPin color={colors.zinc[400]} size={20} />
-					<Input.Field placeholder="Para onde?" />
+					<Input.Field placeholder="Para onde?" editable={isTripDetails} />
 				</Input>
 
 				<Input>
 					<Calendar color={colors.zinc[400]} size={20} />
-					<Input.Field placeholder="Quando?" />
+					<Input.Field placeholder="Quando?" editable={isTripDetails} />
 				</Input>
 
-				<Separator>
-					<Button variant="secondary">
-						<Button.Title>Alterar local / data</Button.Title>
-						<Settings2 color={colors.zinc[200]} size={20} />
-					</Button>
-				</Separator>
+				{stepForm === StepForm.addEmail && (
+					<>
+						<Separator>
+							<Button variant="secondary" onPress={() => setStepForm(StepForm.tripDetails)}>
+								<Button.Title>Alterar local / data</Button.Title>
+								<Settings2 color={colors.zinc[200]} size={20} />
+							</Button>
+						</Separator>
 
-				<Input>
-					<UserRoundPlus color={colors.zinc[400]} size={20} />
-					<Input.Field placeholder="Quem estará na viagem?" />
-				</Input>
+						<Input>
+							<UserRoundPlus color={colors.zinc[400]} size={20} />
+							<Input.Field placeholder="Quem estará na viagem?" />
+						</Input>
+					</>
+				)}
 
-				<Button>
-					<Button.Title>Continuar</Button.Title>
+				<Button onPress={() => setStepForm(StepForm.addEmail)}>
+					<Button.Title>{isTripDetails ? "Continuar" : "Confirmar Viagem"}</Button.Title>
 					<ArrowRight color={colors.lime[950]} size={20} />
 				</Button>
 			</View>

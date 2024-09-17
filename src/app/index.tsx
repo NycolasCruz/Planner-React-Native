6a18c822946a815b2c0d55b7f5a7e8819970c883
NycolasCruz <nycolascruzsilva@gmail.com>
2024-09-17 11:29:02 -0300

    feat: first screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import { Image, Text, View } from "react-native";
 
-import { MapPin, Calendar as CalendarIcon } from "lucide-react-native";
+import { MapPin, Calendar, Settings2, UserRoundPlus, ArrowRight } from "lucide-react-native";
 
 import { Separator } from "@/components/Separator";
 import { Button } from "@/components/Button";
@@ -10,11 +10,13 @@ import logo from "@/assets/logo.png";
 import { colors } from "@/global/colors";
 
 export default function App() {
+	// 11:30
+
 	return (
 		<View className="flex-1 items-center justify-center px-5">
 			<Image source={logo} className="h-8" resizeMode="contain" />
 
-			<Text className="text-zinc-400 font-regular text-center text-lg mt-3">
+			<Text className="text-zinc-400 text-center text-lg mt-3">
 				Convide seus amigos e planeje sua{"\n"}próxima viagem
 			</Text>
 
@@ -23,17 +25,34 @@ export default function App() {
 					<MapPin color={colors.zinc[400]} size={20} />
 					<Input.Field placeholder="Para onde?" />
 				</Input>
+
 				<Input>
-					<CalendarIcon color={colors.zinc[400]} size={20} />
-					<Input.Field placeholder="Para onde?" />
+					<Calendar color={colors.zinc[400]} size={20} />
+					<Input.Field placeholder="Quando?" />
 				</Input>
 
 				<Separator>
-					<Button>
+					<Button variant="secondary">
 						<Button.Title>Alterar local / data</Button.Title>
+						<Settings2 color={colors.zinc[200]} size={20} />
 					</Button>
 				</Separator>
+
+				<Input>
+					<UserRoundPlus color={colors.zinc[400]} size={20} />
+					<Input.Field placeholder="Quem estará na viagem?" />
+				</Input>
+
+				<Button>
+					<Button.Title>Continuar</Button.Title>
+					<ArrowRight color={colors.lime[950]} size={20} />
+				</Button>
 			</View>
+
+			<Text className="text-zinc-500 text-center text-base">
+				Ao planejar sua viagem pela plann.er, você automaticamente concorda com nossos{" "}
+				<Text className="text-zinc-300 underline">termos de uso e políticas de privacidade</Text>
+			</Text>
 		</View>
 	);
 }

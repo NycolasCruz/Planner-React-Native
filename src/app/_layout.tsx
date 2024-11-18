f4c28f6feb8cfa88ed41c436de75f2069a13292a
NycolasCruz <nycolascruzsilva@gmail.com>
2024-11-18 14:31:58 -0300

    feat: using loadAsync

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,34 +1,47 @@
 import { View } from "react-native";
+import { useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
 
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { Slot } from "expo-router";
 
-import { Loading } from "@/components/Loading";
-
-import {
-	useFonts,
-	Inter_400Regular,
-	Inter_500Medium,
-	Inter_600SemiBold,
-} from "@expo-google-fonts/inter";
+import { Inter_400Regular, Inter_500Medium, Inter_600SemiBold } from "@expo-google-fonts/inter";
 import "@/global/global.css";
 
-export default function Layout() {
-	const [fontsLoaded] = useFonts({
-		Inter_400Regular,
-		Inter_500Medium,
-		Inter_600SemiBold,
-	});
+SplashScreen.preventAutoHideAsync();
 
-	if (!fontsLoaded) {
-		return <Loading />;
+export default function Layout() {
+	const [appIsReady, setAppIsReady] = useState(false);
+
+	async function prepare() {
+		try {
+			await Font.loadAsync({
+				Inter_400Regular,
+				Inter_500Medium,
+				Inter_600SemiBold,
+			});
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setAppIsReady(true);
+		}
 	}
+
+	useEffect(() => {
+		prepare();
+	}, []);
+
+	useEffect(() => {
+		if (appIsReady) {
+			SplashScreen.hideAsync();
+		}
+	}, [appIsReady]);
 
 	return (
 		<View className="flex-1 bg-zinc-950">
 			<StatusBar backgroundColor="transparent" style="auto" translucent />
-
 			<GestureHandlerRootView>
 				<Slot />
 			</GestureHandlerRootView>
